00527e9cc5377c683abf1cb1b190a1ee5c966f91
aniakanl <aniakanl@uncc.edu>
2017-10-08 17:26:56 -0400

    refactor(source structure): create two source paths (main and test)

---
+++ src/test/com/aniakanl/http2/frame/test/TestFrameFlag.java
@@ -0,0 +1,14 @@
+package com.aniakanl.http2.frame.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFrameFlag {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
